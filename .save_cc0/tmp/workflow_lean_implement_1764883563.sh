3be91f59959d37b0daff7e657c21f5fd316ba8e3
benbrastmckie <benbrastmckie@gmail.com>
2025-12-16 10:40:55 -0800

    remove claude

--- .save_cc0/tmp/workflow_lean_implement_1764883563.sh
+++
@@ -1,3 +0,0 @@
-export CLAUDE_PROJECT_DIR="/home/benjamin/Documents/Philosophy/Projects/ProofChecker"
-export WORKFLOW_ID="lean_implement_1764883563"
-export STATE_FILE="/home/benjamin/Documents/Philosophy/Projects/ProofChecker/.claude/tmp/workflow_lean_implement_1764883563.sh"
